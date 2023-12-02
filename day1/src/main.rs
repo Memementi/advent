1a64767cf98586fc62390e0e0e2d859a7945f442
Matthew Rolph <matthew.rolph@student.manchester.ac.uk>
2023-12-02 11:53:53 +0000

    Take input file path

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please provide a filename");
+        return;
+    }
+    let filename = &args[1];
+
+    println!("In file: {}", filename);
 }
