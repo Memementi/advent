7568e7aa523602761442718bc20a178ef15b5e9d
Matthew Rolph <matthew.rolph@student.manchester.ac.uk>
2023-12-02 12:07:12 +0000

    Loop over lines with BufReader

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+use std::io::{self, BufRead};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,4 +11,24 @@ fn main() {
     let filename = &args[1];
 
     println!("In file: {}", filename);
+
+    if let Ok(lines) = read_lines(filename) {
+        let mut calibration_total = 0;
+        for line in lines {
+            if let Ok(text) = line {
+                // calibration_total +=
+            }
+        }
+        println!("Total: {}", calibration_total);
+    }
 }
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+// fn get_calibration_value(line: String) -> i32 {
+
+// }
